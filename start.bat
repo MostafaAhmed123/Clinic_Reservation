b2daa94d707dee4e896b4fd642179517cdf2e359
Mostafa Ahmed <m.a7med82002@gmail.com>
2023-12-11 10:45:20 +0200

    initial version for phase 3

--- start.bat
+++ start.bat
@@ -17,11 +17,6 @@ REM creating needed networks
 docker network create dbbackend1
 
 REM running containers
-docker run -d --name backend -p 8000:8000 --net dbbackend1 backend:final
 docker run -d --name database -p 3306:3306 --net dbbackend1 -e MYSQL_ROOT_PASSWORD=Hana.2002 -e MYSQL_DATABASE=clinic database:v1
+docker run -d --name backend -p 8000:8000 --net dbbackend1 --restart always backend:final
 docker run -d --name frontend -p 4200:4200 frontend:final
-@REM docker restart backend
-@REM @REM REM connecting backend with database 
-@REM @REM docker network connect dbbackend1 backend
-@REM @REM docker restart backend
-@REM @REM timeout /nobreak /t 5
